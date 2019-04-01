c0e3b070ea05ed49be274aa547764dbe7c43e83d
laos0 <isonylao@gmail.com>
2019-04-01 00:08:01 -0500

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Animator anim;
+    private float speed;
+    
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        speed = 1.2f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        // For jumpping
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+             anim.SetBool("isJumping", true);
+        }
+        else
+        {
+            anim.SetBool("isJumping", false);
+            // For jogging/running
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                if (Input.GetKey(KeyCode.W))
+                {
+                    anim.SetBool("isJogging", true);
+                    speed = 3f;
+                }
+                else
+                {
+                    anim.SetBool("isJogging", false);
+                }
+
+            }
+            else
+            {
+                anim.SetBool("isJogging", false);
+                speed = 1.2f;
+            }
+
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                //anim.Play("Female Walk");
+                anim.SetBool("isWalking", true);
+
+                gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+                // if player walks then press shift, run
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    anim.SetBool("isWalking", false);
+                    if (Input.GetKey(KeyCode.W))
+                    {
+                        anim.SetBool("isJogging", true);
+                        speed = 3f;
+
+                    }
+                    else
+                    {
+                        anim.SetBool("isJogging", false);
+                    }
+
+                }
+                else
+                {
+                    anim.SetBool("isJogging", false);
+                    speed = 1.2f;
+                }
+
+            }
+            else if (Input.GetKeyUp(KeyCode.W))
+            {
+                anim.SetBool("isWalking", false);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                gameObject.transform.Rotate(0, -speed, 0);
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                gameObject.transform.Rotate(0, speed, 0);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerTestController.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTestController : MonoBehaviour {
+
+    private Animator anim;
+    private float speed;
+    private bool isWalking;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        speed = 1.2f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // after this animation is finished
+            if (!anim.GetBool("isJumping") && anim.GetCurrentAnimatorStateInfo(0).IsName("Blend Tree"))
+            {
+                StartCoroutine("playJumpAnim");
+            }
+
+        }
+        else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
+        {
+
+            if (!anim.GetBool("isJumping"))
+            {
+                //anim.SetBool("isMoving", true);
+                // if it is not jumping then run
+                //anim.SetFloat("blendX", 1f);
+                //anim.SetFloat("blendY", 0f);
+                anim.SetFloat("Blend", 2f);
+
+                speed = 3.0f;
+
+
+                gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+            }
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            if (!anim.GetBool("isJumping"))
+            {
+                anim.SetBool("isMoving", true);
+                
+                //isWalking = true;
+
+                // if it is not jumping walk
+                //anim.SetFloat("blendX", 1f);
+                //anim.SetFloat("blendY", 0f);
+                anim.SetFloat("Blend", 1f);
+                /*
+                if(isWalking == true)
+                {
+                    speed = 1.2f;
+                }
+                else
+                {
+                    speed = 3.0f;
+                }
+                */
+                speed = 1.2f;
+                gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+            }
+
+        }
+        else
+        {
+
+            //anim.SetBool("isMoving", false);
+            //anim.SetFloat("blendX", 0f);
+            //anim.SetFloat("blendY", 0f);
+            anim.SetFloat("Blend", 0f);
+            
+        }
+
+        // For turning the character
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.Rotate(0, -speed, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.Rotate(0, speed, 0);
+        }
+
+
+
+    }
+
+    IEnumerator playJumpAnim()
+    {
+        anim.SetBool("isJumping", true);
+
+        yield return new WaitForEndOfFrame();
+
+        while (!anim.GetCurrentAnimatorStateInfo(0).IsName("Blend Tree"))
+        {
+            yield return null;
+        }
+
+        anim.SetBool("isJumping", false);
+
+    }
+
+    /* Original code
+      if (Input.GetKey(KeyCode.Space))
+        {
+            // when jumping
+            anim.SetFloat("blendX", -1);
+            anim.SetFloat("blendY", 0);
+            
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                isWalking = true;
+                anim.SetFloat("blendX", 1); // walking
+                anim.SetFloat("blendY", 0f);
+               
+                // if shift is also pressed down, then run
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    //anim.SetFloat("Blend", 1f);
+                    //isWalking = false;
+                }
+
+                if (isWalking == true)
+                {
+                    speed = 1.2f;
+                }
+                else
+                {
+                    speed = 3.0f;
+                }
+
+                gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            }
+            else
+            {
+               
+            }
+
+            // These are for taunting
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                anim.SetFloat("Blend", 1.25f);
+            }
+
+            // The turning of the character
+            if (Input.GetKey(KeyCode.A))
+            {
+                gameObject.transform.Rotate(0, -speed, 0);
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                gameObject.transform.Rotate(0, speed, 0);
+            }
+
+        }
+     
+     
+     */
+
+}
