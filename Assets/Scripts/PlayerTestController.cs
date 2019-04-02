df12c4fd731dc77847baeea3908166c26933cee9
laos0 <isonylao@gmail.com>
2019-04-01 23:16:40 -0500

    character movement

--- Assets/Scripts/PlayerTestController.cs
+++ Assets/Scripts/PlayerTestController.cs
@@ -55,24 +55,32 @@ public class PlayerTestController : MonoBehaviour {
 
             if (!anim.GetBool("isJumping"))
             {
+
                 if (!isRun)
                 {
+                    // is running
                     animCounter = 0f;
                     isIdle = false;
-                    isWalk = false;
+                    isWalk = false; 
                     isRun = true;
+                    Debug.Log("is running");
                 }
 
-                speed = 3f;
+                speed = 2f;
 
                 animCounter += Time.deltaTime;
                 targetAnimationValue = runValue;
+                //Debug.Log(runValue);
                 if (animationValue < targetAnimationValue)
                 {
-
+                    //Debug.Log(animationValue);
                     animationValue = .5f + animCurve.Evaluate(animCounter * interpolateSpd);
 
                     anim.SetFloat("Blend", animationValue);
+                }
+                else
+                {
+                    anim.SetFloat("Blend", targetAnimationValue);
                 }
 
 
@@ -83,15 +91,18 @@ public class PlayerTestController : MonoBehaviour {
         }
         else if (Input.GetKey(KeyCode.W))
         {
+
             if (!isWalk)
             {
+                Debug.Log("Is walking");
                 animCounter = 0f;
                 isWalk = true;
                 isRun = false;
                 isIdle = false;
+                
             }
 
-            speed = 2f;
+            speed = 1f;
 
             animCounter += Time.deltaTime;
             targetAnimationValue = walkValue;
@@ -101,8 +112,18 @@ public class PlayerTestController : MonoBehaviour {
                 
                 anim.SetFloat("Blend", animationValue);
             }
+            else
+            {
+                anim.SetFloat("Blend", walkValue);
+            }
             gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
             
+        }else if (Input.GetKey(KeyCode.O)){
+            // Chicken dance
+            if (!anim.GetBool("isDancing") && anim.GetCurrentAnimatorStateInfo(0).IsName("Blend Tree"))
+            {
+                StartCoroutine("playDanceAnim");
+            }
         }
         else
         {
@@ -121,10 +142,10 @@ public class PlayerTestController : MonoBehaviour {
 
                 anim.SetFloat("Blend", animationValue);
             }
-
-            //anim.SetFloat("Blend", 0f);
-            //animCounter = 0f;
-            //animationValue = 0f;
+            else
+            {
+                anim.SetFloat("Blend", idleValue);
+            }
             
         }
 
@@ -158,65 +179,18 @@ public class PlayerTestController : MonoBehaviour {
 
     }
 
-    /* Original code
-      if (Input.GetKey(KeyCode.Space))
+    IEnumerable playDanceAnim()
+    {
+        anim.SetBool("isDancing", true);
+
+        yield return new WaitForEndOfFrame();
+
+        while (!anim.GetCurrentAnimatorStateInfo(0).IsName("Blend Tree"))
         {
-            // when jumping
-            anim.SetFloat("blendX", -1);
-            anim.SetFloat("blendY", 0);
-            
+            yield return null;
         }
-        else
-        {
-            if (Input.GetKey(KeyCode.W))
-            {
-                isWalking = true;
-                anim.SetFloat("blendX", 1); // walking
-                anim.SetFloat("blendY", 0f);
-               
-                // if shift is also pressed down, then run
-                if (Input.GetKey(KeyCode.LeftShift))
-                {
-                    //anim.SetFloat("Blend", 1f);
-                    //isWalking = false;
-                }
 
-                if (isWalking == true)
-                {
-                    speed = 1.2f;
-                }
-                else
-                {
-                    speed = 3.0f;
-                }
-
-                gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
-            }
-            else
-            {
-               
-            }
-
-            // These are for taunting
-            if (Input.GetKeyDown(KeyCode.Alpha1))
-            {
-                anim.SetFloat("Blend", 1.25f);
-            }
-
-            // The turning of the character
-            if (Input.GetKey(KeyCode.A))
-            {
-                gameObject.transform.Rotate(0, -speed, 0);
-            }
-
-            if (Input.GetKey(KeyCode.D))
-            {
-                gameObject.transform.Rotate(0, speed, 0);
-            }
-
-        }
-     
-     
-     */
+        anim.SetBool("isDancing", false);
+    }
 
 }
